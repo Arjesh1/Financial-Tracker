6ba96c100dca03649a3378fc5846efd00c6dded1
Arjesh <arjesh.khadka2@gmail.com>
2023-03-15 08:55:18 +1100

    2nd comit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+
+      <Button>hello<i class="fa-solid fa-house"></i></Button>
+
+      
+
       
     </div>
   );
